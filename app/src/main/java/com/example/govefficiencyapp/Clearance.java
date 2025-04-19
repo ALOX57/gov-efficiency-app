f1c2aa51d2ae262ff1b4cf6910a075941d62096e
Alexander Birtles <aloxemail@gmail.com>
2025-04-19 12:41:24 +1000

    Add Clearance skeleton class

---
+++ app/src/main/java/com/example/govefficiencyapp/Clearance.java
@@ -0,0 +1,11 @@
+package com.example.govefficiencyapp;
+
+public class Clearance {
+    private String clearanceType;
+    private int duration;
+
+    public Clearance(String clearanceType, int duration){
+        this.clearanceType = clearanceType;
+        this.duration = duration;
+    }
+}
