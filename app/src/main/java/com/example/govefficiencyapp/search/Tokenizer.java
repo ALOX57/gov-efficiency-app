7e485947997103601ed96d177025307deff958d0
Alexander Birtles <aloxemail@gmail.com>
2025-04-19 13:10:12 +1000

    Add Tokenizer skeleton class

---
+++ app/src/main/java/com/example/govefficiencyapp/search/Tokenizer.java
@@ -0,0 +1,14 @@
+package com.example.govefficiencyapp.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tokenizer {
+    public static List<String> tokenise(String input){
+
+        // TODO: implement logic
+
+        List<String> tokens = new ArrayList<>();
+        return tokens;
+    }
+}
