4ea71e939e0563e5961c0335f807f6da965a8a6c
Alexander Birtles <aloxemail@gmail.com>
2025-04-19 13:11:16 +1000

    Add Parser skeleton class

---
+++ app/src/main/java/com/example/govefficiencyapp/search/Parser.java
@@ -0,0 +1,10 @@
+package com.example.govefficiencyapp.search;
+
+import java.util.List;
+public class Parser {
+    public static Query parse(List<String> tokens) {
+        // TODO: implement logic
+
+        return null;
+    }
+}
