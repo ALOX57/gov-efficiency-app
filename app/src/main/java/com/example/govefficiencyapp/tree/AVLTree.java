c4aa7d87b8d53d81f9b603b392e2526df05a0a3b
Alexander Birtles <aloxemail@gmail.com>
2025-04-19 13:36:17 +1000

    Add AVLTree skeleton class

---
+++ app/src/main/java/com/example/govefficiencyapp/tree/AVLTree.java
@@ -0,0 +1,26 @@
+package com.example.govefficiencyapp.tree;
+
+
+import android.app.Application;
+
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * AVL Tree for storing applications sorted by date.
+ */
+
+public class AVLTree {
+    private TreeNode root;
+
+    public void insert(Application app){
+        // TODO: Insert logic
+    }
+
+    public List<Application> getApplicationsByDate(LocalDate date) {
+        // TODO: Search tree for date
+        return null;
+    }
+
+
+}
