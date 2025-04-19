10e77934aecea2cc00ecb446bd0e6cfb82295947
Alexander Birtles <aloxemail@gmail.com>
2025-04-19 12:53:24 +1000

    Add Department skeleton class

---
+++ app/src/main/java/com/example/govefficiencyapp/domain/Department.java
@@ -0,0 +1,11 @@
+package com.example.govefficiencyapp.domain;
+
+public class Department {
+    private String departmentName;
+    private int departmentID;
+
+    public Department(String departmentName, int departmentID) {
+        this.departmentName = departmentName;
+        this.departmentID = departmentID;
+    }
+}
