1fcca1c1e5b4d12eca53a2b03fe6b7f8455ef719
Alexander Birtles <aloxemail@gmail.com>
2025-04-19 12:51:46 +1000

    Add status field to Application class

--- app/src/main/java/com/example/govefficiencyapp/domain/Application.java
+++ app/src/main/java/com/example/govefficiencyapp/domain/Application.java
@@ -1,5 +1,7 @@
 package com.example.govefficiencyapp.domain;
 
+import com.example.govefficiencyapp.enums.Status;
+
 public class Application {
     private String startDate;
     private String endDate;
@@ -7,13 +9,15 @@ public class Application {
     private String approvalDate;
     private Clearance clearance;
     private int id;
+    private Status status;
 
-    public Application(String startDate, String endDate, String requestDate, String approvalDate, Clearance clearance, int id){
+    public Application(String startDate, String endDate, String requestDate, String approvalDate, Clearance clearance, int id, Status status){
         this.startDate = startDate;
         this.endDate = endDate;
         this.requestDate = requestDate;
         this.approvalDate = approvalDate;
         this.clearance = clearance;
         this.id = id;
+        this.status = status;
     }
 }
