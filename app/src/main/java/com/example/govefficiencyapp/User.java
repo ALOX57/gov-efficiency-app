b4d8ef8fcabec17d8e6c1f7f44f65fd2c8484c84
Alexander Birtles <aloxemail@gmail.com>
2025-04-19 12:39:26 +1000

    Add User skeleton class

---
+++ app/src/main/java/com/example/govefficiencyapp/User.java
@@ -0,0 +1,14 @@
+package com.example.govefficiencyapp;
+
+public class User {
+    private int id;
+    private String name;
+    private boolean isAdmin;
+
+    public User(int id, String name, boolean isAdmin){
+        this.id = id;
+        this.name = name;
+        this.isAdmin = isAdmin;
+    }
+
+}
