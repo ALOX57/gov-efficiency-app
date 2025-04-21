96bfa66c6fac20d6ce21f7cd0755dc93b7538546
Timothy Tsang <u7614415@anu.edu.au>
2025-04-21 16:00:46 +1000

    Delete FormFactory.java

--- app/src/main/java/com/example/govefficiencyapp/forms/FormFactory.java
+++
@@ -1,7 +0,0 @@
-package com.example.govefficiencyapp.forms;
-
-import com.example.govefficiencyapp.domain.User;
-
-public abstract class FormFactory {
-    public abstract Form createForm(User user);
-}
