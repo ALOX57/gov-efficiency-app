1aad6fe9b12657172318e8cc862c12eea8fa7d44
Timothy Tsang <u7614415@anu.edu.au>
2025-04-21 16:00:41 +1000

    Delete Form.java

--- app/src/main/java/com/example/govefficiencyapp/forms/Form.java
+++
@@ -1,9 +0,0 @@
-package com.example.govefficiencyapp.forms;
-
-import java.util.Map;
-
-public interface Form {
-    Map<String, String> getRequiredFields();
-    Map<String, String> getSubmittedFields();
-    String getTitle();
-}
