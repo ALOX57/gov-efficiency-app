a102b80b8d9721d04c444c1c51a3cb5233d0b183
Timothy Tsang <u7614415@anu.edu.au>
2025-04-21 16:01:28 +1000

    Upload New File

---
+++ app/src/main/java/com/example/govefficiencyapp/forms/FormFactory.java
@@ -0,0 +1,8 @@
+package com.example.govefficiencyapp.forms;
+
+import com.example.govefficiencyapp.domain.User;
+
+public interface FormFactory {
+    Form createTier1Form();
+    Form createTier2Form();
+}
