37de44441b2b9dcc518543cdc48384ae40a4064a
= <u7923257@anu.edu.au>
2025-04-22 19:48:35 +1000

    Added the skeleton for ApplicationDetails

---
+++ app/src/main/java/com/example/govefficiencyapp/application/ApplicationDetails.java
@@ -0,0 +1,21 @@
+package com.example.govefficiencyapp.application;
+
+import com.example.govefficiencyapp.forms.EducationDetails;
+import com.example.govefficiencyapp.forms.FamilyDetails;
+import com.example.govefficiencyapp.forms.PersonalDetails;
+
+public class ApplicationDetails {
+    public PersonalDetails personalDetails;
+    public FamilyDetails familyDetails;
+    public EducationDetails educationDetails;
+    public String residentialHist;
+    public String employmentHist;
+    public String criminalHist;
+    public String clearanceReason;
+    public int requestedDuration;
+
+
+    public void manipulateDetails(){
+
+    }
+}
