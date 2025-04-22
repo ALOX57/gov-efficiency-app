d940a5c2846716f4c3fa28c8d79ac2f4d198d4a2
= <u7923257@anu.edu.au>
2025-04-22 22:05:28 +1000

    Added the skeleton for ApplicationDetails

--- app/src/main/java/com/example/govefficiencyapp/application/ApplicationDetails.java
+++ app/src/main/java/com/example/govefficiencyapp/application/ApplicationDetails.java
@@ -1,5 +1,8 @@
 package com.example.govefficiencyapp.application;
 
+import com.example.govefficiencyapp.domain.Application;
+import com.example.govefficiencyapp.domain.Clearance;
+import com.example.govefficiencyapp.enums.Status;
 import com.example.govefficiencyapp.forms.EducationDetails;
 import com.example.govefficiencyapp.forms.FamilyDetails;
 import com.example.govefficiencyapp.forms.PersonalDetails;
@@ -14,7 +17,17 @@ public class ApplicationDetails {
     public String clearanceReason;
     public int requestedDuration;
 
+    public ApplicationDetails (PersonalDetails personalDetails, FamilyDetails familyDetails, EducationDetails educationDetails, String residentialHist, String employmentHist, String criminalHist, String clearanceReason, int requestedDuration) {
+        this.personalDetails = personalDetails;
+        this.familyDetails = familyDetails;
+        this.educationDetails = educationDetails;
+        this.residentialHist = residentialHist;
+        this.employmentHist = employmentHist;
+        this.criminalHist = criminalHist;
+        this.clearanceReason = clearanceReason;
+        this.requestedDuration = requestedDuration;
 
+    }
     public void manipulateDetails(){
 
     }
