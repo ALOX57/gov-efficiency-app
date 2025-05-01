fcd7adac78e92e7d1c6d078510be9ff0047a5a72
TheMacSpawn <stickcrafter03@gmail.com>
2025-05-01 13:21:00 +1000

    Edited User.java to be closer to UML specs. Added simple salting to passwords to improve security.

--- app/src/main/java/com/example/govefficiencyapp/domain/User.java
+++ app/src/main/java/com/example/govefficiencyapp/domain/User.java
@@ -1,23 +1,44 @@
 package com.example.govefficiencyapp.domain;
 
+import java.time.Instant;
+import java.util.Date;
+
 public class User {
-    private int id;
+    final private int id;
     private String name;
-    private boolean isAdmin;
-    private int password;
+    final private boolean isAdmin;
+    private String password;
+    private String registerDate;
+    private int loginStatus = 0;
 
     public User(int id, String name, boolean isAdmin){
         this.id = id;
         this.name = name;
         this.isAdmin = isAdmin;
+        this.registerDate = Date.from(Instant.now()).toString();// Might want to get registerDate with predetermined dates later
+
+    }
+
+    public void login(){
+        loginStatus = 1;
+    }
+
+    public void logout(){
+        loginStatus = 0;
+    }
+
+    public int getLoginStatus(){
+        return loginStatus;
     }
 
     public void setPassword(String password) {
-        this.password = password.hashCode();
+        String saltedPassword = password + String.valueOf(this.id);
+        this.password = String.valueOf(saltedPassword.hashCode());
     }
 
     public boolean checkPassword(String enteredPassword){
-        return this.password==enteredPassword.hashCode();
+        String saltedPassword = enteredPassword + String.valueOf(this.id);
+        return this.password.equals(String.valueOf(saltedPassword.hashCode()));
     }
 
     public String getName() {
@@ -25,5 +46,21 @@ public class User {
     }
     public boolean getAdminStatus(){
         return isAdmin;
+    }
+
+    public String getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(String newRegisterDate){
+        /*
+        I think this should be removed later. There isn't any reason to change the register date
+        after we've finished testing
+         */
+        this.registerDate = newRegisterDate;
+    }
+
+    public int getId() {
+        return id;
     }
 }
